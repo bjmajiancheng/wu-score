d036107e2fd4c84be3ea40ccb2715eac142f6dc9
xueguangrui <10504228262qq.com>
2019-01-24 09:07:49 +0800

    改测试账号

--- src/main/java/com/wutuobang/common/controller/SysLoginController.java
+++ src/main/java/com/wutuobang/common/controller/SysLoginController.java
@@ -98,7 +98,7 @@ public class SysLoginController {
         /*
         qiyezhanghao1是个申请人用户名，用来测试用的
          */
-        if(!username.equals("qiyezhanghao1")){
+        if(!username.equals("tianjindawan")){
             if (flag){
                 return ResultParam.error("此时间段不受理积分落户，请关注重要通知！");
             }
