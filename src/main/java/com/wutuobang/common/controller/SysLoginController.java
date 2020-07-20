876d501467966ac2247b0c37317ef8789fcd7b6f
xueguangrui <10504228262qq.com>
2020-07-20 11:21:21 +0800

    Update SysLoginController.java

--- src/main/java/com/wutuobang/common/controller/SysLoginController.java
+++ src/main/java/com/wutuobang/common/controller/SysLoginController.java
@@ -113,7 +113,13 @@ public class SysLoginController {
         /*
         qiyezhanghao1是个申请人用户名，用来测试用的
          */
-        if(!username.equals("QIYEZHANGHAO1")){
+        boolean userFlage = false;
+        if(username.equals("QIYEZHANGHAO1")){
+            userFlage = true;
+        }else if(username.equals("QIYEZHANGHAO2")){
+            userFlage = true;
+        }
+        if(!userFlage){
             if (flag) {
                 return ResultParam.error("此时间段不受理积分落户，请关注重要通知！");
             }
